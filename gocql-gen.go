ee64444f72c1305fe37f45c7702ccae0645b7adb
TimTheSinner <timthesinner@gmail.com>
2016-07-15 11:21:08 -0600

    Finished refactor to clean model and template functions, added ability to ser/der to cassandra blobs

--- gocql-gen.go
+++ gocql-gen.go
@@ -26,9 +26,10 @@ func Usage() {
 }
 
 type columnDef struct {
-	Name    string `json:"name"`
-	CqlType string `json:"type"`
-	Key     string `json:"key"`
+	Name                string `json:"name"`
+	CqlType             string `json:"type"`
+	Key                 string `json:"key"`
+	DeserializeFromBlob string `json:"deserializeTo"`
 }
 
 func (c *columnDef) String() string {
@@ -61,10 +62,8 @@ func main() {
 			os.Exit(1)
 		}
 
-		model := _DAOModel{Arguments: strings.Join(os.Args[1:], " "), Package: "TBD", AdditionalImports: "TBD", Model: *model, DAO: *dao}
+		model := _DAOModel{Package: "TBD", AdditionalImports: "TBD", Model: *model, DAO: *dao}
 
-		clusteringColumns := []string{}
-		clusteringOrder := []string{}
 		for _, col := range columns {
 			//model.Columns = append(model.Columns, col.Name+" "+col.CqlType)
 			switch col.Key {
@@ -72,15 +71,15 @@ func main() {
 				model.partitioningKeys = append(model.partitioningKeys, col.Name)
 				model.keys = append(model.keys, col.Name)
 			case "cluster", "cluster-asc", "cluster-desc":
-				clusteringColumns = append(clusteringColumns, col.Name)
+				model.clusteringKeys = append(model.clusteringKeys, col.Name)
 				model.keys = append(model.keys, col.Name)
 			}
 
 			switch col.Key {
 			case "cluster-asc":
-				clusteringOrder = append(clusteringOrder, col.Name+" ASC")
+				model.clusteringOrder = append(model.clusteringOrder, col.Name+" ASC")
 			case "cluster-desc":
-				clusteringOrder = append(clusteringOrder, col.Name+" DESC")
+				model.clusteringOrder = append(model.clusteringOrder, col.Name+" DESC")
 			}
 
 			column := &param{Name: col.Name, CqlType: col.CqlType}
@@ -89,15 +88,17 @@ func main() {
 				column.GoType = "string"
 			case "uuid", "timeuuid":
 				column.GoType = "*gocql.UUID"
+			case "list<blob>":
+				column.GoType = "[][]byte"
+				column.SerializedType = col.DeserializeFromBlob
+			case "map<string,blob>":
+				column.GoType = "map[string][]byte"
+				column.SerializedType = col.DeserializeFromBlob
 			default:
 				column.GoType = "UNKNOWN"
 			}
 			model.Columns = append(model.Columns, column)
 		}
-
-		model.InitPartitioningKeys()
-		model.InitClusteringColumns(clusteringColumns)
-		model.InitClusteringOrder(clusteringOrder)
 
 		if template, err := template.New("DaoTemplate").Parse(_DAOTemplate); err != nil {
 			log.Fatalf("DAOTemplate was not legal: %v", err)
@@ -109,61 +110,63 @@ func main() {
 }
 
 type param struct {
-	Name    string
-	GoType  string
-	CqlType string
+	Name           string
+	GoType         string
+	CqlType        string
+	SerializedType string
 }
 
 type _DAOModel struct {
-	Arguments         string
 	Package           string
 	AdditionalImports string
 	Model             string
 	ModelImport       string
 	DAO               string
 
-	Keyspace          string
-	Table             string
-	PartioningKeys    string
-	ClusteringColumns string
-	ClusteringOrder   string
-	Columns           []*param
+	Keyspace string
+	Table    string
+	Columns  []*param
 
 	partitioningKeys []string
+	clusteringKeys   []string
+	clusteringOrder  []string
 	keys             []string
+}
+
+func (m _DAOModel) Arguments() template.HTML {
+	return template.HTML(strings.Join(os.Args[1:], " "))
 }
 
 func (m _DAOModel) TableDefinition() template.HTML {
 	params := make([]string, len(m.Columns))
 	for i, p := range m.Columns {
-		params[i] = p.Name + " " + p.CqlType
+		params[i] = fmt.Sprintf("    %v %v", p.Name, p.CqlType)
 	}
-	return template.HTML(strings.Join(params, ", "))
+	return template.HTML(strings.Join(params, ",\n"))
 }
 
-func (m *_DAOModel) InitPartitioningKeys() {
+func (m _DAOModel) PartitioningKeys() template.HTML {
 	if len(m.partitioningKeys) == 0 {
 		log.Fatal("Partitioning keys were empty")
 		os.Exit(1)
 	} else if len(m.partitioningKeys) == 1 {
-		m.PartioningKeys = m.partitioningKeys[0]
-	} else {
-		m.PartioningKeys = fmt.Sprintf("(%v)", strings.Join(m.partitioningKeys, ", "))
+		return template.HTML(m.partitioningKeys[0])
 	}
+	return template.HTML(fmt.Sprintf("(%v)", strings.Join(m.partitioningKeys, ", ")))
 }
 
-func (m *_DAOModel) InitClusteringColumns(keys []string) {
-	if len(keys) == 0 {
-		return
+func (m _DAOModel) ClusteringColumns() template.HTML {
+	if len(m.clusteringKeys) == 0 {
+		return template.HTML("")
 	}
-	m.ClusteringColumns = fmt.Sprintf(", %v", strings.Join(keys, ", "))
+	return template.HTML(fmt.Sprintf(", %v", strings.Join(m.clusteringKeys, ", ")))
 }
 
-func (m *_DAOModel) InitClusteringOrder(keys []string) {
-	if len(keys) == 0 {
-		return
+func (m _DAOModel) ClusteringOrder() template.HTML {
+	if len(m.clusteringOrder) == 0 {
+		return template.HTML("")
 	}
-	m.ClusteringOrder = fmt.Sprintf(" WITH CLUSTERING ORDER BY (%v)", strings.Join(keys, ", "))
+	return template.HTML(fmt.Sprintf(" WITH CLUSTERING ORDER BY (%v)", strings.Join(m.clusteringOrder, ", ")))
 }
 
 func (m _DAOModel) GetScanParameters() template.HTML {
@@ -202,12 +205,16 @@ func (m _DAOModel) InsertValues() template.HTML {
 func (m _DAOModel) InsertResource() template.HTML {
 	params := make([]string, len(m.Columns))
 	for i, p := range m.Columns {
-		params[i] = "r." + p.Name
+		if p.SerializedType == "" {
+			params[i] = "r." + p.Name
+		} else {
+			params[i] = p.Name
+		}
 	}
 	return template.HTML(strings.Join(params, ", "))
 }
 
-func (m _DAOModel) GetAllKeys() template.HTML {
+func (m _DAOModel) SelectSingleKeys() template.HTML {
 	return template.HTML(strings.Join(m.keys, ", "))
 }
 
@@ -219,7 +226,7 @@ func (m _DAOModel) SelectSingle() template.HTML {
 	return template.HTML(strings.Join(keys, " AND "))
 }
 
-func (m _DAOModel) GetKeys() template.HTML {
+func (m _DAOModel) SelectListKeys() template.HTML {
 	return template.HTML(strings.Join(m.partitioningKeys, ", "))
 }
 
@@ -231,8 +238,75 @@ func (m _DAOModel) SelectList() template.HTML {
 	return template.HTML(strings.Join(keys, " AND "))
 }
 
-const _DAOTemplate = `
-// Code generated by "gocql-gen {{.Arguments}}"; DO NOT EDIT THIS FILE
+func (m _DAOModel) CreateResourceFromParameters() template.HTML {
+	resource := make([]string, len(m.Columns))
+	for i, c := range m.Columns {
+		if c.SerializedType == "" {
+			resource[i] = fmt.Sprintf("          %v: %v", c.Name, c.Name)
+		} else if c.CqlType == "list<blob>" {
+			resource[i] = fmt.Sprintf("          %v: make([]%v, 0)", c.Name, c.SerializedType)
+		} else if c.CqlType == "map<string,blob>" {
+			resource[i] = fmt.Sprintf("          %v: make(map[string]%v)", c.Name, c.SerializedType)
+		}
+	}
+	return template.HTML(strings.Join(resource, ",\n") + ",")
+}
+
+func (m _DAOModel) DeserializeParameters() template.HTML {
+	deser := make([]string, 0)
+	for _, c := range m.Columns {
+		if c.SerializedType != "" {
+			if c.CqlType == "list<blob>" {
+				deser = append(deser, fmt.Sprintf(`
+    for _, v := range %v {
+      var value %v
+      json.Unmarshal(v, &value)
+      resource.%v = append(resource.%v, value)
+    }`, c.Name, c.SerializedType, c.Name, c.Name))
+			} else if c.CqlType == "map<string,blob>" {
+				deser = append(deser, fmt.Sprintf(`
+    for k, v := range %v {
+      var value %v
+      json.Unmarshal(v, &value)
+      resource.%v[k] = value
+    }`, c.Name, c.SerializedType, c.Name))
+			}
+		}
+	}
+	return template.HTML(strings.Join(deser, "\n"))
+}
+
+func (m _DAOModel) SerializeParameters() template.HTML {
+	ser := make([]string, 0)
+	for _, c := range m.Columns {
+		if c.SerializedType != "" {
+			if c.CqlType == "list<blob>" {
+				ser = append(ser, fmt.Sprintf(`
+  %v := make([][]byte, 0)
+  for _, v := range resource.%v {
+    if value, err := json.Marshal(v); err == nil {
+      %v = append(%v, value)
+    } else {
+      fmt.Println("Could not marshal value:", err, v)
+    }
+  }`, c.Name, c.Name, c.Name, c.Name))
+			} else if c.CqlType == "map<string,blob>" {
+				ser = append(ser, fmt.Sprintf(`
+  %v := make(map[string][]byte)
+  for k, v := range resource.%v {
+    if value, err := json.Marshal(v); err == nil {
+      %v[k] = value
+    } else {
+      fmt.Println("Could not marshal attribute:", k, err, v)
+    }
+  }`, c.Name, c.Name, c.Name))
+			}
+		}
+	}
+	return template.HTML(strings.Join(ser, "\n") + "\n")
+}
+
+const _DAOTemplate = `// Code generated by "gocql-gen {{.Arguments}}"; DO NOT EDIT THIS FILE
 /*
  *
  * Model that generated this code: {{.RawJSON}}
@@ -261,21 +335,21 @@ type {{.Model}}Stream struct {
 
 func (dao *{{.DAO}}) Init(session *gocql.Session) (error) {
   return session.Query(` + "`" + `CREATE TABLE IF NOT EXISTS {{.Keyspace}}.{{.Table}} (
-    {{.TableDefinition}},
+{{.TableDefinition}},
 
-    PRIMARY KEY ({{.PartioningKeys}}{{.ClusteringColumns}})
+    PRIMARY KEY ({{.PartitioningKeys}}{{.ClusteringColumns}})
   ){{.ClusteringOrder}};` + "`" + `).Exec()
 }
 
-func (dao *{{.DAO}}) Get({{.GetAllKeys}} interface{}, _session ...*gocql.Session) (*{{.ModelImport}}{{.Model}}, error) {
+func (dao *{{.DAO}}) Get({{.SelectSingleKeys}} interface{}, _session ...*gocql.Session) (*{{.ModelImport}}{{.Model}}, error) {
   session, err, close := dao.session(_session...)
-	if err != nil {
-		return nil, err
-	} else if close {
-		defer session.Close()
-	}
+  if err != nil {
+    return nil, err
+  } else if close {
+    defer session.Close()
+  }
 
-  if res, err := dao.list(session, ` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectSingle}};` + "`" + `, {{.GetKeys}}); err != nil {
+  if res, err := dao.list(session, ` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectSingle}};` + "`" + `, {{.SelectSingleKeys}}); err != nil {
     return nil, err
   } else if len(res) != 1 {
     return nil, nil
@@ -284,33 +358,33 @@ func (dao *{{.DAO}}) Get({{.GetAllKeys}} interface{}, _session ...*gocql.Session
   }
 }
 
-func (dao *{{.DAO}}) List({{.GetKeys}} interface{}, _session ...*gocql.Session) ([]*{{.ModelImport}}{{.Model}}, error) {
+func (dao *{{.DAO}}) List({{.SelectListKeys}} interface{}, _session ...*gocql.Session) ([]*{{.ModelImport}}{{.Model}}, error) {
   session, err, close := dao.session(_session...)
-	if err != nil {
-		return nil, err
-	} else if close {
-		defer session.Close()
-	}
+  if err != nil {
+    return nil, err
+  } else if close {
+    defer session.Close()
+  }
 
-  return dao.list(session, ` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectList}};` + "`" + `, {{.GetKeys}})
+  return dao.list(session, ` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectList}};` + "`" + `, {{.SelectListKeys}})
 }
 
-func (dao *{{.DAO}}) Stream({{.GetKeys}} interface{}) chan *{{.Model}}Stream {
-  return dao.stream(` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectList}};` + "`" + `, {{.GetKeys}})
+func (dao *{{.DAO}}) Stream({{.SelectListKeys}} interface{}) chan *{{.Model}}Stream {
+  return dao.stream(` + "`" + `SELECT {{.InsertFields}} FROM {{.Keyspace}}.{{.Table}} WHERE {{.SelectList}};` + "`" + `, {{.SelectListKeys}})
 }
 
 func (dao *{{.DAO}}) session(_session ...*gocql.Session) (*gocql.Session, error, bool) {
-	if _session == nil || len(_session) != 1 || _session[0] == nil {
-		if session, err := dao.CreateSession(); err != nil {
-			return nil, err, false
-		} else {
-			return session, nil, true
-		}
-	}
-	return _session[0], nil, false
+  if _session == nil || len(_session) != 1 || _session[0] == nil {
+    if session, err := dao.CreateSession(); err != nil {
+      return nil, err, false
+    } else {
+      return session, nil, true
+    }
+  }
+  return _session[0], nil, false
 }
 
-func (dao *{{.DAO}}) add(r *{{.ModelImport}}{{.Model}}, session *gocql.Session) (*{{.ModelImport}}{{.Model}}, error) {
+func (dao *{{.DAO}}) add(r *{{.ModelImport}}{{.Model}}, session *gocql.Session) (*{{.ModelImport}}{{.Model}}, error) { {{.SerializeParameters}}
   err := session.Query(` + "`" + `INSERT INTO {{.Keyspace}}.{{.Table}} ({{.InsertFields}})
                       VALUES ({{.InsertValues}});` + "`" + `,
                       {{.InsertResource}}).Exec()
@@ -321,12 +395,13 @@ func (dao *{{.DAO}}) add(r *{{.ModelImport}}{{.Model}}, session *gocql.Session) 
 }
 
 func (dao *{{.DAO}}) stream(cql string, params ...interface{}) chan *{{.Model}}Stream {
-  stream := make(chan *{{.Model}}Stream, 4)
+  stream := make(chan *{{.Model}}Stream, dao.capacity())
 
   go func() {
     defer close(stream)
 
     if session, err := dao.CreateSession(); err != nil {
+      fmt.Println("Could not initialize sesion to stream resources for {{.Table}}", err)
       {{.EmitStream}}{DTO: nil, ERR: err}
     } else {
       defer session.Close()
@@ -339,12 +414,15 @@ func (dao *{{.DAO}}) stream(cql string, params ...interface{}) chan *{{.Model}}S
       iter := session.Query(cql, params...).Iter()
       for iter.Scan({{.GetScanParameters}}) {
         resource := &{{.ModelImport}}{{.Model}}{
-          {{range .Columns}}{{.Name}}: {{.Name}},
-          {{end}}}
+{{.CreateResourceFromParameters}}
+        }
+        {{.DeserializeParameters}}
+
         {{.EmitStream}}{DTO: resource, ERR: nil}
       }
 
       if err := iter.Close(); err != nil {
+        fmt.Println("Error streaming resources for {{.Table}}", cql, err)
         {{.EmitStream}}{DTO: nil, ERR: err}
       }
     }
@@ -359,19 +437,22 @@ func (dao *{{.DAO}}) list(session *gocql.Session, cql string, params ...interfac
     {{end}})
 
   iter := session.Query(cql, params...).Iter()
-  results := make([]*{{.ModelImport}}{{.Model}}, 4)
+  results := make([]*{{.ModelImport}}{{.Model}}, dao.capacity())
   for iter.Scan({{.GetScanParameters}}) {
     resource := &{{.ModelImport}}{{.Model}}{
-      {{range .Columns}}{{.Name}}: {{.Name}},
-      {{end}}}
+{{.CreateResourceFromParameters}}
+    }
+    {{.DeserializeParameters}}
+
     results = append(results, resource)
   }
 
   if err := iter.Close(); err != nil {
-    fmt.Println(err)
+    fmt.Println("Error listing resources for {{.Table}}", cql, err)
     return nil, err
   }
 
   return results, nil
 }
+
 `
